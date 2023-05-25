47c6bea0ff31ce9b504a910a3723efb53fd107e4
Shawn <crucifix8016@gmail.com>
2023-05-25 15:43:14 +0800

    test

--- src/main/java/Shawn.java
+++ src/main/java/Shawn.java
@@ -3,6 +3,10 @@ public class Shawn {
 
 	public static void main(String[] args) {
 		System.out.println("分支測試~~~");
+		System.out.println("分支測試~~~");
+		System.out.println("分支測試~~~");
+		System.out.println("分支測試~~~");
+		System.out.println("分支測試~~~");
 	}
 	
 	
