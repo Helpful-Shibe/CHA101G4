026d6def484fa072977375b2b2ffb704af30dd3a
COCO <ssrichi171@gmail.com>
2023-05-25 15:43:27 +0800

    add branch

--- src/main/java/Coco.java
+++ src/main/java/Coco.java
@@ -3,7 +3,7 @@ public class Coco {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("HelloWorld!");
+		System.out.println("HelloWorld!分支ver");
 	}
 
 }
