63b6cdef4855ff81d116b1870d9c28ff9062af3f
COCO <ssrichi171@gmail.com>
2023-05-25 16:03:50 +0800

    add2

--- src/main/java/Coco.java
+++ src/main/java/Coco.java
@@ -3,7 +3,7 @@ public class Coco {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("HelloWorld!分支ver");
+		System.out.println("HelloWorld!分支ver2");
 	}
 
 }
