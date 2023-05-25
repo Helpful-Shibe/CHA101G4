975298f6a4907af03537840b095887e56b3be686
Leo <leopeter800@gmail.com>
2023-05-25 14:44:56 +0800

    改檔名

--- src/main/java/Leo.java
+++ src/main/java/Leo.java
@@ -7,8 +7,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/TestServlet2")
-public class TestServlet2 extends HttpServlet {
+@WebServlet("/Leo")
+public class Leo extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 
